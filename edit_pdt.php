f1924f6c1da11116fda8c4bb144c7c1da5ab109d
VInicius-Mance <vinimanceprof@gmail.com>
2020-05-10 15:31:14 -0300

    edição de produto
    Função de editar produtos adicionada

--- edit_pdt.php
+++ edit_pdt.php
@@ -4,7 +4,31 @@ session_start();
 if (!$_SESSION) {
 header('location: login.php');
 }
- ?>
+$produto = call_pdt($_GET['id']);
+foreach($produto as $pdt){}
+if ($_POST) {
+  $pdt = $_POST['pdt'];
+  $preco = $_POST['preco'];
+  $descricao = $_POST['descricao'];
+  $foto = $produto['foto'];
+if ($produto) {
+    $files = fetch_pdt();
+    foreach($files as $item => $info) {
+      if($info['id'] == $_GET['id']) {
+      $new_info = ['produto'=>$pdt,'preco'=>$preco,'foto'=>$foto,'descricao'=>$descricao];
+      $new_file = array_replace($files[$item],$new_info);
+      unset($files[$item]);
+      array_values($new_file);
+      $files[]= $new_file;
+      $data = json_encode($files);
+      file_put_contents('data.json', $data);
+      header("location: edit_pdt.php?id=".$_GET['id']);
+      break;
+      }
+    }
+  }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,22 +38,20 @@ header('location: login.php');
 </head>
 <body>
 <div class="site">
-<?php include('header.php');
-$produto = call_pdt($_GET['id']);
-foreach ($produto as $pdt) {} ?>
+<?php include('header.php');?>
 <div class="container">
-  <form action="item.php?id=<?php echo $produto['id'];?>" method="post" enctype="multipart/form-data">
+  <form method="post" enctype="multipart/form-data">
     <label for="pdt">Nome</label><br>
-      <input type="text" name="novo_pdt" value="<?php echo $produto['produto'];?>"><br>
+      <input type="text" name="pdt" value="<?php echo $produto['produto'];?>"><br>
     <label for="descricao">Descrição do produto</label><br>
-      <input type="text" name="novo_descricao" value="<?php echo $produto['descricao']; ?>"><br>
+      <input type="text" name="descricao" value="<?php echo $produto['descricao']; ?>"><br>
     <label for="preco">Preço do Produto</label><br>
-      <input type="number" name="novo_preco" value="<?php echo $produto['preco']; ?>"><br>
+      <input type="number" name="preco" value="<?php echo $produto['preco']; ?>"><br>
     <label for="foto">Foto</label><br>
     <img src="<?php echo $produto['foto'];?>" alt="">
-      <input type="file" name="novo_foto"><br>
+      <input type="file" name="foto"><br>
     <button type="submit" name="button">Salvar alterações</button>
-    <a href="item.php?id=<?php echo $produto['id'];?>">Voltar e descartar alterações</a>
+    <a href="item.php?id=<?php echo $produto['id'];?>">Voltar ao item</a>
   </form>
       </div>
     </div>
